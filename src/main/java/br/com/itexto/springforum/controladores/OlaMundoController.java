6a8f1ab98b7ced936056ad97d1e3ecab10b3c717
btd1337 <helder.bertoldo@ice.ufjf.br>
2017-02-09 13:21:32 -0200

    Olá spring

--- src/main/java/br/com/itexto/springforum/controladores/OlaMundoController.java
+++ src/main/java/br/com/itexto/springforum/controladores/OlaMundoController.java
@@ -3,15 +3,15 @@ package br.com.itexto.springforum.controladores;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OlaMundoController {
+	
 	@RequestMapping("/ola")
-	public ModelAndView olaMundo() {
-		ModelAndView resultado = new ModelAndView("Mundo");
-		resultado.addObject("data", new Date());
-		return resultado;
+	public String olaMundo(Model model) {
+		model.addAttribute("data", new Date());
+		return "mundo";
 	}
 }
